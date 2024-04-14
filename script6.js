b709e1058e470c8c6bc971f8e916fc48a6da34d1
LukasDrakon <90932532+LukasDrakon@users.noreply.github.com>
2024-04-13 23:09:11 -0400

    Update script6.js

--- script6.js
+++ script6.js
@@ -1019,7 +1019,7 @@ function generate_final_map(){
         //create random landsacpe
         var height = []
         for (var i=0; i<10; i++)
-            height.push(Math.floor(Math.random()*3)+5)
+            height.push(Math.floor(Math.random()*3)+6)
 
         //create cspin hole
         var is_right = Math.floor(Math.random()*2) == 0 //overhang is left?
